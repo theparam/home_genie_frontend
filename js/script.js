ff8e635f7cb4b80fb0bc4dd6047c430f67089273
Shubham Verma <vshubham799@gmail.com>
2022-02-06 18:11:48 -0800

    pop up notification work done. Need real implementation

--- js/script.js
+++ js/script.js
@@ -35,9 +35,23 @@ document.addEventListener("keydown", (e) => {
   if (!e.repeat) {
     if (e.key === "Escape") {
       document.getElementById("showHideMenu").classList.remove("menu_active");
+      document
+        .querySelector(".NotificationPopUpContainer")
+        .classList.remove("popUPActive");
     }
   }
 });
 document.getElementById("ShowMenu").addEventListener("click", () => {
   document.getElementById("showHideMenu").classList.toggle("menu_active");
+  document
+    .querySelector(".NotificationPopUpContainer")
+    .classList.remove("popUPActive");
+});
+
+let bellIcon = document.getElementById("ShowNotificationPopUp");
+bellIcon.addEventListener("click", () => {
+  document
+    .querySelector(".NotificationPopUpContainer")
+    .classList.toggle("popUPActive");
+  document.getElementById("showHideMenu").classList.remove("menu_active");
 });
