e3605139213d4f77204d43a514ac2fc6264f1e49
Shubham Verma <vshubham799@gmail.com>
2022-02-11 18:26:26 -0800

    Added login change made by tanya to my branch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-let loginBtn = document.getElementById("LoginBtn");
 let bellIcon = document.getElementById("ShowNotificationPopUp");
 
 document.addEventListener("keydown", (e) => {
@@ -24,48 +23,4 @@ bellIcon.addEventListener("click", () => {
     .querySelector(".NotificationPopUpContainer")
     .classList.toggle("popUPActive");
   document.getElementById("showHideMenu").classList.remove("menu_active");
-});
-
-loginBtn.addEventListener("click", () => {
-  // e.preventDefault();
-  let emailId = document.getElementById("email").value;
-  let paswd = document.getElementById("password").value;
-
-  const response = new XMLHttpRequest();
-  const json = JSON.stringify({
-    email: emailId,
-    password: paswd,
-  }); //
-
-  const url = "http://localhost:8080/home/genie/user/login";
-  const data = { title: "The Matrix", year: "1994" };
-  //
-  fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "*",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "POST",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: json,
-    method: "POST",
-  })
-    .then("data => data.json()))")
-    .then((response) => {
-      alert("Response: " + response.status);
-      response.json();
-    })
-    .then((user) => {
-      myStorage = window.sessionStorage;
-      myStorage.setItem("LoggedInUser", JSON.stringify(user));
-      alert("Login Successful");
-      alert("user:" + user);
-      alert("user:" + JSON.stringify(user));
-      window.location.href = "/html_Files/index.html";
-      console.log("Data: " + user);
-    })
-    .catch((err) => {
-      alert("Error: " + err);
-    });
 });
