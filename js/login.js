de244d16d471483ec509154433e064db6208e21b
Shubham Verma <vshubham799@gmail.com>
2022-02-12 00:23:20 -0800

    Login and Sign up integration with apis done

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
 let loginBtn = document.getElementById("LoginBtn");
+const url = "http://localhost:8080/home/genie/user/login";
+const UserSessionStorageKey = "LoggedInUser";
 
-loginBtn.addEventListener("click", () => {
-  alert("hi");
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   // e.preventDefault();
-  let emailId = document.getElementById("login-username").value;
+  let emailId = document.getElementById("login-email").value;
   let paswd = document.getElementById("login-password").value;
 
   const response = new XMLHttpRequest();
@@ -12,10 +14,54 @@ loginBtn.addEventListener("click", () => {
     password: paswd,
   }); //
 
-  const url = "http://localhost:8080/home/genie/user/login";
-  const data = { title: "The Matrix", year: "1994" };
+  postData(url, json)
+    .then((userData) => {
+      // User Data from DB
+      let logInUser = JSON.stringify(userData);
+      console.log("Data: " + logInUser);
+
+      alert("Login Successful");
+      UpdateStorageSessions(UserSessionStorageKey, logInUser);
+      window.location.href = "/html_Files/index.html";
+    })
+    .catch((err) => {
+      alert(err);
+    });
+
   //
-  fetch(url, {
+  // fetch(url, {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Accept: "*",
+  //     "Access-Control-Allow-Credentials": "true",
+  //     "Access-Control-Allow-Methods": "POST",
+  //     "Access-Control-Allow-Origin": "*",
+  //   },
+  //   body: json,
+  //   method: "POST",
+  // })
+  //   .then("data => data.json()))")
+  //   .then((response) => {
+  //     alert("Response: " + response.status);
+  //     response.json();
+  //   })
+  //   .then((user) => {
+  //     myStorage = window.sessionStorage;
+  //     myStorage.setItem("LoggedInUser", JSON.stringify(user));
+  //     alert("Login Successful");
+  //     alert("user:" + user);
+  //     alert("user:" + JSON.stringify(user));
+  //     window.location.href = "/html_Files/index.html";
+  //     console.log("Data: " + user);
+  //   })
+  //   .catch((err) => {
+  //     alert("Error: " + err);
+  //   });
+});
+
+async function postData(url = "", data) {
+  // Default options are marked with *
+  const response = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
       Accept: "*",
@@ -23,24 +69,21 @@ loginBtn.addEventListener("click", () => {
       "Access-Control-Allow-Methods": "POST",
       "Access-Control-Allow-Origin": "*",
     },
-    body: json,
+    body: data,
     method: "POST",
-  })
-    .then("data => data.json()))")
-    .then((response) => {
-      alert("Response: " + response.status);
-      response.json();
-    })
-    .then((user) => {
-      myStorage = window.sessionStorage;
-      myStorage.setItem("LoggedInUser", JSON.stringify(user));
-      alert("Login Successful");
-      alert("user:" + user);
-      alert("user:" + JSON.stringify(user));
-      window.location.href = "/html_Files/index.html";
-      console.log("Data: " + user);
-    })
-    .catch((err) => {
-      alert("Error: " + err);
-    });
-});
+  }).catch((err) => {
+    return err;
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+const UpdateStorageSessions = (key, value) => {
+  myStorage = window.sessionStorage;
+
+  if (myStorage.getItem(key) == null) {
+    myStorage.setItem(key, value);
+  } else {
+    myStorage.removeItem(key);
+    myStorage.setItem(key, value);
+  }
+};
---
+++ js/signup.js
@@ -0,0 +1,100 @@
+let signUpBtn = document.getElementById("SignUpBtn");
+const url = "http://localhost:8080/home/genie/user/signup";
+const UserSessionStorageKey = "LoggedInUser";
+
+class signUpUser {
+  constructor(firstName, lastName, phoneNumber, email, paswd) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.password = paswd;
+  }
+}
+
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let fName = document.getElementById("SignUp_fName").value;
+  let lName = document.getElementById("SignUp_lName").value;
+  let phoneNumber = document.getElementById("SignUp_phoneNumber").value;
+  let emailId = document.getElementById("SignUp_email").value;
+  let pwd = document.getElementById("SignUp_password").value;
+  let cpwd = document.getElementById("SignUp_cPassword").value;
+  if (
+    fName.toString() === "" ||
+    lName.toString() === "" ||
+    emailId.toString() === "" ||
+    pwd.toString() === "" ||
+    cpwd.toString() === ""
+  ) {
+    alert("Fields cannot be blank");
+    return;
+  } else if (pwd.toString() == cpwd.toString()) {
+    //   creating user obj
+    let signUp_User = new signUpUser(fName, lName, phoneNumber, emailId, pwd);
+
+    const json = JSON.stringify(signUp_User);
+
+    postData(url, json)
+      .then((userData) => {
+        // User Data from DB
+        let logInUser = JSON.stringify(userData);
+        console.log("Data: " + logInUser);
+
+        alert("Login Successful");
+        UpdateStorageSessions(UserSessionStorageKey, logInUser);
+        window.location.href = "/html_Files/index.html";
+      })
+      .catch((err) => {
+        alert(err);
+      });
+
+    // let userData = fetch(url, {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Accept: "*",
+    //     "Access-Control-Allow-Credentials": "true",
+    //     "Access-Control-Allow-Methods": "POST",
+    //     "Access-Control-Allow-Origin": "*",
+    //   },
+    //   body: json,
+    //   method: "POST",
+    // })
+    //   .then((response) => {
+    //     alert("Response: " + response.status);
+    //     return response.json();
+    //   })
+    //   .catch((err) => {
+    //     return err;
+    //   });
+  } else {
+    document.querySelector(".errorContainr").classList.add("active");
+  }
+});
+
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: data,
+    method: "POST",
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+const UpdateStorageSessions = (key, value) => {
+  myStorage = window.sessionStorage;
+
+  if (myStorage.getItem(key) == null) {
+    myStorage.setItem(key, value);
+  } else {
+    myStorage.removeItem(key);
+    myStorage.setItem(key, value);
+  }
+};
