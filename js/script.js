68138e37cfbc3399efbbe94cacd0b65c22138916
Shubham Verma <vshubham799@gmail.com>
2022-02-11 17:18:43 -0800

    removed the scss and kept one css file only

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
-function UserAction() {
-  // console.log(e);
-  // e.preventDefault();
-  fname = document.getElementById("firstname").value;
-  lname = document.getElementById("lastname").value;
-  emailId = document.getElementById("email").value;
-  paswd = document.getElementById("password").value;
-  console.log(fname);
-  const response = new XMLHttpRequest();
+let loginBtn = document.getElementById("LoginBtn");
+let bellIcon = document.getElementById("ShowNotificationPopUp");
 
+document.addEventListener("keydown", (e) => {
+  if (!e.repeat) {
+    if (e.key === "Escape") {
+      document.getElementById("showHideMenu").classList.remove("menu_active");
+      document
+        .querySelector(".NotificationPopUpContainer")
+        .classList.remove("popUPActive");
+    }
+  }
+});
+
+document.getElementById("ShowMenu").addEventListener("click", () => {
+  document.getElementById("showHideMenu").classList.toggle("menu_active");
+  document
+    .querySelector(".NotificationPopUpContainer")
+    .classList.remove("popUPActive");
+});
+
+bellIcon.addEventListener("click", () => {
+  document
+    .querySelector(".NotificationPopUpContainer")
+    .classList.toggle("popUPActive");
+  document.getElementById("showHideMenu").classList.remove("menu_active");
+});
+
+loginBtn.addEventListener("click", () => {
+  // e.preventDefault();
+  let emailId = document.getElementById("email").value;
+  let paswd = document.getElementById("password").value;
+
+  const response = new XMLHttpRequest();
   const json = JSON.stringify({
-    firstName: fname,
-    lastName: lname,
     email: emailId,
     password: paswd,
   }); //
-  const url = "http://localhost:8080/home/genie/user/signup";
+
+  const url = "http://localhost:8080/home/genie/user/login";
   const data = { title: "The Matrix", year: "1994" };
   //
   fetch(url, {
@@ -29,29 +52,20 @@ function UserAction() {
     method: "POST",
   })
     .then("data => data.json()))")
-    .then(alert("Success:"));
-}
-document.addEventListener("keydown", (e) => {
-  if (!e.repeat) {
-    if (e.key === "Escape") {
-      document.getElementById("showHideMenu").classList.remove("menu_active");
-      document
-        .querySelector(".NotificationPopUpContainer")
-        .classList.remove("popUPActive");
-    }
-  }
-});
-document.getElementById("ShowMenu").addEventListener("click", () => {
-  document.getElementById("showHideMenu").classList.toggle("menu_active");
-  document
-    .querySelector(".NotificationPopUpContainer")
-    .classList.remove("popUPActive");
-});
-
-let bellIcon = document.getElementById("ShowNotificationPopUp");
-bellIcon.addEventListener("click", () => {
-  document
-    .querySelector(".NotificationPopUpContainer")
-    .classList.toggle("popUPActive");
-  document.getElementById("showHideMenu").classList.remove("menu_active");
+    .then((response) => {
+      alert("Response: " + response.status);
+      response.json();
+    })
+    .then((user) => {
+      myStorage = window.sessionStorage;
+      myStorage.setItem("LoggedInUser", JSON.stringify(user));
+      alert("Login Successful");
+      alert("user:" + user);
+      alert("user:" + JSON.stringify(user));
+      window.location.href = "/html_Files/index.html";
+      console.log("Data: " + user);
+    })
+    .catch((err) => {
+      alert("Error: " + err);
+    });
 });
