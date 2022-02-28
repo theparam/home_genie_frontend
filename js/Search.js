c23c157ed10d7060eef213c8bdeed7dbd84e8c61
Shubham Verma <vshubham799@gmail.com>
2022-02-28 00:01:14 -0800

    Changes merged from all branches to master branch

--- js/Search.js
+++ js/Search.js
@@ -27,28 +27,41 @@ window.addEventListener("load", (e) => {
   ).innerHTML = `You Searched for: <span id='searchTitle'>${searchValue}</span>`;
   searchValue = encodeURIComponent(searchValue.trim());
   let searchQuery = searchValue + "/" + categoryValue;
-  url = url + searchQuery;
+
+  let searchparam = {
+    title: searchValue,
+    category: categoryValue,
+  };
+
+  let jsonObj = JSON.stringify(searchparam);
+
+  url = url + `?title=${searchValue}` + `&category=${categoryValue}`;
   postData(url)
     .then((searchedData) => {
       let SearchArray = JSON.stringify(searchedData);
-
+      console.log("respnos " + SearchArray);
       for (let i = 0; i < searchedData.length; i++) {
         let searchedListing = searchedData[i];
-        console.log("Data: " + searchedListing);
+        console.log("Data: " + JSON.stringify(searchedListing));
         let searchCardResult = `<div class="card">
          <img src="https://picsum.photos/500/250?random=11" alt="random image">
          <h3>${searchedListing.title}</h3>
          <p>Posted By <span>${searchedListing.listingOwner}</span></p>
          <div class="browseBtn_Container">
-             <a href="#" class="browseBtn">View Listing</a>
+             <a href="#" class="browseBtn" id="${searchedListing.id}" onclick="ViewListing(this);">View Listing</a>
          </div>`;
-        Search_Listing_Container.innerHTML = searchCardResult;
+        Search_Listing_Container.innerHTML += searchCardResult;
       }
     })
     .catch((err) => {
       alert(err);
     });
 });
+
+let ViewListing = (btnObject) => {
+  console.log("SpecificListing.html?ListingId=" + btnObject.id);
+  window.location.href = "SpecificListing.html?ListingId=" + btnObject.id;
+};
 
 async function postData(url = "") {
   // Default options are marked with *
