5eaf456f0b3f901fb9d37e3d59f9f3511168be08
Shubham Verma <vshubham799@gmail.com>
2022-03-16 16:49:47 -0700

    Added the notification Coding Work. Now I need some rest. Remaining is the setting the status

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let fetchBidOffers = "http://localhost:8080/home/genie/user/bid/";
 
 let fetchNotificationUrl = "http://localhost:8080/home/genie/user/";
 
+let notificationArray = [];
 let loggdInUser = "";
 let bellIcon = document.getElementById("ShowNotificationPopUp");
 let dk_bellIcom = document.getElementById("dk-ShowNotificationPopUp");
@@ -19,6 +20,7 @@ if (loggedInUser != null) {
   loggdInUser = JSON.parse(loggedInUser);
 }
 
+// When index page is loaded.
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("mb_user_Name").innerHTML = document.getElementById(
     "dk_user_Name"
@@ -26,11 +28,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   GetNotification("Owner");
   GetNotification("Customer");
+
+  // Some code require async await
+  // if (notificationArray.length > 3) {
+  //   for (let a = 0; a < notificationArray.length; a++) {
+  //     if (a > 2) {
+  //       break;
+  //     } else {
+  //       document.querySelector(".NotificationPopUp").innerHTML +=
+  //         notificationArray[a];
+  //     }
+  //   }
+  // } else {
+  //   for (let a = 0; a < notificationArray.length; a++) {
+  //     document.querySelector(".NotificationPopUp").innerHTML +=
+  //       notificationArray[a];
+  //   }
+  // }
 });
 
 function GetNotification(user) {
   let userId = loggdInUser.id;
-
+  let iconFlag = false;
   let url = "";
   if (user === "Owner") {
     url = fetchNotificationUrl + "getOwnerNotifications/" + userId;
@@ -40,20 +59,127 @@ function GetNotification(user) {
 
   fetchNotificationForUser(url)
     .then((data) => {
-      console.log("Data: " + JSON.stringify(data));
+      for (let n = 0; n < data.length; n++) {
+        if (data[n].status == "unread") {
+          iconFlag = true;
+          console.log(iconFlag);
+        }
+        if (user == "Owner") {
+          GenerateNoticationforUserAsOwner(data[n]);
+        } else {
+          GenerateNoticationforUserAsCustomer(data[n]);
+        }
+      }
 
-      // GenerateNotificationContainer();
+      if (iconFlag) {
+        document.getElementById("dk_NotificationGenie").src =
+          document.getElementById("mb_NotificationGenie").src =
+            "../img/New_Notification_Genie.png";
+      }
     })
     .catch((err) => {
       console.log("error: " + err);
     });
 }
 
+// Generate notification contaiers for the Owner
+function GenerateNoticationforUserAsOwner(notificationData) {
+  let urlListing = listingURL + notificationData.listingId;
+
+  console.log("Owner url " + urlListing);
+  getData(urlListing)
+    .then((listingRes) => {
+      getBidOfferDataForUser(notificationData.bidOfferId)
+        .then((bidRes) => {
+          let ownerNotification = getOwnerNotificationContainer(
+            notificationData,
+            listingRes,
+            bidRes
+          );
+
+          // notificationArray.push(ownerNotification);
+          document.querySelector(".NotificationPopUp").innerHTML +=
+            ownerNotification;
+        })
+        .catch((err) => {
+          console.log("Error: " + err);
+        });
+    })
+    .catch((err) => {
+      console.log("Error: " + err);
+    });
+}
+
+// Generate notification contaiers for the Owner
+function GenerateNoticationforUserAsCustomer(notificationData) {
+  let urlListing = listingURL + notificationData.listingId;
+
+  console.log("url customer " + urlListing);
+  getData(urlListing)
+    .then((listingRes) => {
+      console.log("listingRes: " + listingRes.ownerUserId);
+      getData(fetchNotificationUrl + listingRes.ownerUserId)
+        .then((owner) => {
+          console.log("Owner: " + owner);
+          let customerNotification = getCustomerNotificationContainer(
+            notificationData,
+            listingRes,
+            owner
+          );
+          // notificationArray.push(customerNotification);
+          document.querySelector(".NotificationPopUp").innerHTML +=
+            customerNotification;
+        })
+        .catch((err) => {
+          console.log("Error: " + err);
+        });
+    })
+    .catch((err) => {
+      console.log("Error: " + err);
+    });
+}
+
+function getOwnerNotificationContainer(
+  notificationData,
+  listing,
+  biddingOffer
+) {
+  let container = ` <div class="ownerNotification">
+     <h3 class="listingTitle">${listing.title}</h1>
+       <p>You have received a new offer by <span id="custonerName">${
+         biddingOffer.bidUserName
+       }</span></p>
+       <div class="viewbtnWrapper">
+         <a class="viewlisting" id="${
+           listing.id + "_" + notificationData.id
+         }">View Listing</a>
+       </div>
+   </div>`;
+  return container;
+}
+function getCustomerNotificationContainer(notificationData, listing, owner) {
+  let container = `<div class="customerNotification">
+  <h3 class="listingTitle">${listing.title}</h1>
+    <p>Horray!! <span id="ownerName">${
+      owner.firstName
+    }</span> accepted your offer</p>
+    <div class="viewbtnWrapper">
+    <a class="viewlisting" id="${
+      listing.id + "_" + notificationData.id
+    }">View Listing</a>
+    </div>
+</div>`;
+  return container;
+}
+
+// Close the notification on cross click
 document.getElementById("CrossIcon").addEventListener("click", () => {
   document
     .querySelector(".NotificationPopUpContainer")
     .classList.remove("popUPActive");
 });
+
+// Clase all open divs on escape button
 document.addEventListener("keydown", (e) => {
   if (!e.repeat) {
     if (e.key === "Escape") {
@@ -188,55 +314,7 @@ function ShowMessageAndRedirect() {
   window.location.href = "Login.html";
 }
 
-// //#region  get notification when page is opened.
-// window.addEventListener("load", (e) => {
-//   // Send the request to get the data for the notification for the logged in user.
-//   // data will be received from the array itself.
-//   let notification = JSON.parse(notiData);
-//   for (let j = 0; j < notification.length; j++) {
-//     let notificationData = notification[j];
-
-//     let ListingID = notificationData.listingId;
-//     let BiddingID = notificationData.biddingId;
-
-//     // Get the bidding user info first
-
-//     getBiddingOffers(BiddingID).then((bidData) => {
-//       let listinURLWithID = listingURL + ListingID;
-//       fetchListingData(listinURLWithID).then((listingData) => {
-//         let notification = GenerateNotification(
-//           notificationData,
-//           listingData,
-//           bidData
-//         );
-
-//         document.querySelector(".NotificationPopUp").innerHTML += notification;
-//       });
-//     });
-//     // Get the listing as well.
-
-//     console.log("Notification Data: " + notification[j].userId);
-//   }
-// });
-// //#endregion
-
-// let GenerateNotification = (notification, listing, bidData) => {
-//   let notificationContainer = `<div class="Notification_id1">
-//   <h3>${listing.title}</h3>
-//   <p>${bidData.bidUserName} sent you an offer of ${bidData.biddingOffer}</p>
-//   <div>
-//   <button class="browseBtn" id="${listing.id}" onclick="viewNotifiedListing(this);">View Listing</button>
-//   </div>
-// </div>`;
-
-//   return notificationContainer;
-// };
-
-// let viewNotifiedListing = (btnObject) => {
-//   console.log("SpecificListing.html?ListingId=" + btnObject.id);
-//   window.location.href = "SpecificListing.html?ListingId=" + btnObject.id;
-// };
-
+// This method is used to get the notification for the User
 async function fetchNotificationForUser(url) {
   return await fetchNotificationData(url)
     .then((notification) => {
@@ -248,6 +326,36 @@ async function fetchNotificationForUser(url) {
 }
 
 async function fetchNotificationData(url = "") {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Origin": "*",
+    },
+    method: "GET",
+  }).catch((err) => {
+    return err;
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// get bid ofer by id
+
+async function getBidOfferDataForUser(bidOfferId) {
+  let url = fetchBidOffers + bidOfferId;
+  return await getData(url)
+    .then((bidOffer) => {
+      return bidOffer;
+    })
+    .catch((err) => {
+      alert("Error: " + err);
+    });
+}
+
+async function getData(url = "") {
   // Default options are marked with *
   const response = await fetch(url, {
     headers: {
